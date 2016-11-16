1949c5aaaa4b6bd12de26539667fdacfdc0e6e06
jzy <shasj@163.com>
2016-11-16 20:11:41 +0800

    touch a new file

---
+++ include/MaxFlow.h
@@ -0,0 +1,26 @@
+#ifndef __MAXFLOW_H_
+#define __MAXFLOW_H_
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+class MaxFlow{
+public:
+	MaxFlow(){};
+	
+private:
+
+};
+
+
+
+
+
+
+
+
+
+#endif
